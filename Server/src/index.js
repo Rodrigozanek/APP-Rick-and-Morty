e94b3dc6b4b740b0d4a6a84f08300d2a126753fa
rodrigozanek <rodrigozanek22@gmail.com>
2023-04-27 10:13:13 -0300

    Promises completo

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,20 +1,18 @@
 const http = require("http");
+const getCharById = require('./controllers/getCharById')
 // const { url } = require("inspector");    
+// const data = require("./utils/data.js")
 const POST = 3001
-const data = require("./utils/data.js")
 
 http.createServer((req, res)=>{
     res. setHeader('Access-Control-Allow-Origin', '*');
     try {
         const {url} = req
-        if(url.includes("/rickandmorty/character/")){
-            const id = url.split("/").at(-1)
-            // const id = url.split("/").pop() 
-            const character = data.find((char)=>char.id == id) 
-            res.writeHead(200, {"Content-Type":"application/json"})
-            res.end(JSON.stringify(character))
+        if(url.includes("/rickandmorty/character")){
+            const id = url.split("/").pop()
+            getCharById(res, id)
         }
-        
+
     } catch (error) {
         console.log(error)
         throw new Error(error)              
@@ -23,3 +21,17 @@ http.createServer((req, res)=>{
 }).listen(POST, "localhost", ()=>{
     console.log(`server on port ${POST}`)
 })
+    
+
+
+
+
+
+
+    // if(url.includes("/rickandmorty/character/")){
+    //     const id = url.split("/").at(-1)
+    //     // const id = url.split("/").pop() 
+    //     const character = data.find((char)=>char.id == id) 
+    //     res.writeHead(200, {"Content-Type":"application/json"})
+    //     res.end(JSON.stringify(character))
+    // }
